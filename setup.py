af4633ad1bca639cd4e6c2060ad575bcf988b4b8
Rob Lathrop <rl@polyswarm.io>
2018-10-15 15:26:10 -0400

    Use a sub version instead of upping to .2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements():
 
 setup(
     name='polyswarm-client',
-    version='0.2',
+    version='0.1.1',
     description='Client library to simplify interacting with a polyswarmd instance',
     author='PolySwarm Developers',
     author_email='info@polyswarm.io',
