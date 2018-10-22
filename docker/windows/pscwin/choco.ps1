690252cd5420715edda20fbab455e406b9f1b696
Matt <mb@polyswarm.io>
2018-10-22 18:01:25 -0400

    force PS to TLS 1.2

--- docker/windows/pscwin/choco.ps1
+++ docker/windows/pscwin/choco.ps1
@@ -1,6 +1,7 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install python --version 3.5.4 -y
 choco install git -y
