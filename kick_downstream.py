29d4b04ae5581975cc4d1c74312c413a3217ce44
Steve Bassi <sb@polyswarm.io>
2018-11-13 09:20:47 -0400

    missing hanging quote

--- kick_downstream.py
+++ kick_downstream.py
@@ -6,7 +6,7 @@ class DownstreamKicker(object):
     def __init__(self):
         self.gitlab_url_base = "https://gitlab.polyswarm.io"
         # everything that relies on polyswarm-client, both public and private
-        self.dependent_projects = os.getenv("CI_PROP_ENGINES", "").split() + os.getenv("CI_POLYSWARM_CLIENT_DOWNSTREAM").split()
+        self.dependent_projects = os.getenv("CI_PROP_ENGINES", "").split() + os.getenv("CI_POLYSWARM_CLIENT_DOWNSTREAM", "").split()
 
     def kick_dependent_project(self, proj):
         return requests.post("{0}/api/v4/projects/{1}/trigger/pipeline/".format(self.gitlab_url_base,
