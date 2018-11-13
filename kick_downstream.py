069d93a7b4e8f7ef36f37683a7770caae77e6905
Steve Bassi <sb@polyswarm.io>
2018-11-13 12:01:59 -0400

    dumping out.

--- kick_downstream.py
+++ kick_downstream.py
@@ -6,18 +6,23 @@ class DownstreamKicker(object):
     def __init__(self):
         self.gitlab_url_base = "https://gitlab.polyswarm.io"
         # everything that relies on polyswarm-client, both public and private
-        self.dependent_projects = os.getenv("CI_PROP_ENGINES", "").split() + os.getenv("CI_POLYSWARM_CLIENT_DOWNSTREAM",
-                                                                                       "").split()
+        self.dependent_projects = os.getenv("CI_POLYSWARM_CLIENT_DOWNSTREAM","").split()
+
         self.head = {'Private-Token': os.getenv("CI_CUSTOM_CI_PAT")}
 
     def resolve_project_id(self, proj):
-        r = requests.get("{0}/api/v4/projects/".format(self.gitlab_url_base), headers=self.head)
+        j = self._get_all_projects()
         proj_name = "externalci/{}".format(proj)
-        j = r.json()
+
         for p in j:
             if p['path_with_namespace'] == proj_name:
                 return p['id']
         raise Exception("project {0} not found".format(proj_name))
+
+    def _get_all_projects(self):
+        r = requests.get("{0}/api/v4/projects/".format(self.gitlab_url_base), headers=self.head)
+        r.raise_for_status()
+        return r.json()
 
     def get_or_update_project_token(self, project_id):
         url = "{0}/api/v4/projects/{1}/triggers".format(self.gitlab_url_base, project_id)
@@ -30,7 +35,8 @@ class DownstreamKicker(object):
                 return t['token']
 
         r = requests.post(url, data={'description': "kicker"}, headers=self.head)
-        return r.json()['token']
+        j = r.json()
+        return j['token']
 
     def kick_dependent_project(self, proj):
 
@@ -46,14 +52,35 @@ class DownstreamKicker(object):
         r.raise_for_status()
         return r
 
+    def get_engine_projects(self):
+        '''
+        Get all projects that are engines, rebuild.
+        :return: ['engine-ikarus']
+        '''
+        ps = []
+        for p in self._get_all_projects():
+            if p['path_with_namespace'].startswith("externalci/engine-"):
+                ps.append(p['path'])
+        return ps
+
+
     def kick_projects(self):
         for p in self.dependent_projects:
+            self.kick_dependent_project(p)
+
+    def kick_engine_projects(self):
+        # todo command line for this
+        engines = os.getenv("CI_PROP_ENGINES", "").split()
+        if not engines:
+            engines = self.get_engine_projects()
+        for p in engines:
             self.kick_dependent_project(p)
 
 
 def main():
     d = DownstreamKicker()
     d.kick_projects()
+    d.kick_engine_projects()
 
 
 if __name__ == "__main__":
