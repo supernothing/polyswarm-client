c94970a8451eee5290745c3660d65881f474fefd
Steve Bassi <sb@polyswarm.io>
2018-11-13 09:48:21 -0400

    added error handling in kick

--- kick_downstream.py
+++ kick_downstream.py
@@ -9,9 +9,11 @@ class DownstreamKicker(object):
         self.dependent_projects = os.getenv("CI_PROP_ENGINES", "").split() + os.getenv("CI_POLYSWARM_CLIENT_DOWNSTREAM", "").split()
 
     def kick_dependent_project(self, proj):
-        return requests.post("{0}/api/v4/projects/{1}/trigger/pipeline/".format(self.gitlab_url_base,
+        r =  requests.post("{0}/api/v4/projects/{1}/trigger/pipeline/".format(self.gitlab_url_base,
                                                                                 "externalci/{}".format(proj)),
-                             data={"token": os.getenv("CI_JOB_TOKEN")})
+                             data={"token": os.getenv("CI_JOB_TOKEN"), "ref": os.getenv("CI_COMMIT_REF_NAME")})
+        r.raise_for_status()
+        return r
 
     def kick_projects(self):
         for p in self.dependent_projects:
