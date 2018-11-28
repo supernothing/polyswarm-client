4db23a146659b89f4b8e56df31c1c34728ef402e
Nick Davis <nick.davis@polyswarm.io>
2018-11-28 13:28:10 -0500

    removed hard-coded Microengine module names for pre-packaged microengines. All are now covered by the default Microengine class name.

--- src/microengine/__main__.py
+++ src/microengine/__main__.py
@@ -3,11 +3,6 @@ import importlib
 import logging
 import sys
 
-from microengine.clamav import Microengine as ClamavMicroengine
-from microengine.eicar import Microengine as EicarMicroengine
-from microengine.multi import Microengine as MultiMicroengine
-from microengine.scratch import Microengine as ScratchMicroengine
-from microengine.yara import Microengine as YaraMicroengine
 from polyswarmclient.config import init_logging
 
 logger = logging.getLogger(__name__)  # Initialize logger
@@ -25,23 +20,11 @@ def choose_backend(backend):
     Raises:
         (Exception): If backend is not found
     """
-    micro_engine_class = None
-    if backend == 'scratch':
-        micro_engine_class = ScratchMicroengine
-    elif backend == 'eicar':
-        micro_engine_class = EicarMicroengine
-    elif backend == 'clamav':
-        micro_engine_class = ClamavMicroengine
-    elif backend == 'yara':
-        micro_engine_class = YaraMicroengine
-    elif backend == 'multi':
-        micro_engine_class = MultiMicroengine
-    else:
-        import_l = backend.split(":")
-        micro_engine_module_s = import_l[0]
+    import_l = backend.split(":")
+    micro_engine_module_s = import_l[0]
 
-        micro_engine_module = importlib.import_module(micro_engine_module_s)
-        micro_engine_class = micro_engine_module.Microengine if ":" not in backend else getattr(micro_engine_module, import_l[1])
+    micro_engine_module = importlib.import_module(micro_engine_module_s)
+    micro_engine_class = micro_engine_module.Microengine if ":" not in backend else getattr(micro_engine_module, import_l[1])
 
     if micro_engine_class is None:
         raise Exception("No microengine backend found {0}".format(backend))
