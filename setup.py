d6cf4c04626892012da998318756afc4cb9c6d5d
Maxwell Koo <mjkoo90@gmail.com>
2018-12-13 22:25:27 -0700

    Bump version to 1.0rc2 (#120)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='polyswarm-client',
-    version='1.0rc1',
+    version='1.0rc2',
     description='Client library to simplify interacting with a polyswarmd instance',
     long_description=long_description,
     long_description_content_type="text/markdown",
