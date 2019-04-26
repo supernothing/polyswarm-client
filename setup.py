368c50dd16f1b74698bc4b87472b528cabd83282
Nick Davis <nick.davis@polyswarm.io>
2019-04-26 18:01:53 -0400

    removed e2e testing, removed old windows python package builds. bump version to 1.4.2. build new windows image after push to pypi, so it can use the new polyswarm-client version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='polyswarm-client',
-    version='1.4.1',
+    version='1.4.2',
     description='Client library to simplify interacting with a polyswarmd instance',
     long_description=long_description,
     long_description_content_type="text/markdown",
