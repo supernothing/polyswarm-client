ec63f09654e081882476a5a3f8a2bde8b101e555
Rob Lathrop <rl@polyswarm.io>
2019-05-21 10:18:03 -0400

    Fix env var names

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name='polyswarm-client',
-    version='2.0.0rc0',
+    version='2.0.0rc1',
     description='Client library to simplify interacting with a polyswarmd instance',
     long_description=long_description,
     long_description_content_type="text/markdown",
